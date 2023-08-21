77a3a189ff3973953bf038baf7be2b418b2ce550
Guilherme Santiago de Brito <125681424+GuilhermeBritoBR@users.noreply.github.com>
2023-08-21 16:49:57 -0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    printf("Numero: %d \n", 10);
+    printf(" Octal: %o \n", 10);
+    printf("Decimal ponto: %f \n", 10);
+    printf("Éxadecimal: %x \n", 10);
+    printf("Decimal sem sinal: %u \n", 10);
+    printf("Ponteiro: %p \n", 10);
+    printf("Notação cientifica %e \n",10000);
+}
